1b9d99164f3bd6f36e2244b2ac28bd6ed4013d99
Rodrigo B <165532@upf.br>
2021-05-16 14:57:05 -0300

    Criada Classe "ModalidadeSubEvento" e ajustado o persistence.xml

---
+++ GedGrupo4/src/main/java/br/upf/ads/topicos/entities/ModalidadeSubEvento.java
@@ -0,0 +1,32 @@
+package br.upf.ads.topicos.entities;
+
+import static javax.persistence.GenerationType.SEQUENCE;
+
+import java.io.Serializable;
+import java.lang.Integer;
+import java.lang.String;
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+import org.hibernate.validator.constraints.Length;
+
+/**
+ * Entity implementation class for Entity: ModalidadeSubEvento
+ *
+ */
+@Entity
+
+public class ModalidadeSubEvento implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy = SEQUENCE, generator = "ModalidadeSubEventoId")
+	@SequenceGenerator(name = "ModalidadeSubEventoId", allocationSize = 1, initialValue = 1)
+	private Integer id;
+
+	private static final long serialVersionUID = 1L;
+
+	public ModalidadeSubEvento() {
+		super();
+	}
+
+}
